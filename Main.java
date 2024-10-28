a4f73b9a7933890c8bc32eaf9ff4d7b06c6cc425
ViniciusDSB <vinidsb581@gmail.com>
2024-10-28 09:55:19 -0400

    FIrst actual commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+package codego;
+
+import compresses.*;
+import hashes.*;
+
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        //read path
+        //instance txtDealer and use it
+
+        //instance compression and use it
+
+        //ask for hash
+
+        //instance chosen hash and indexes the files
+
+    }
+}
---
+++ TxtDealer.java
@@ -0,0 +1,11 @@
+package codego;
+
+//import the think to deal with the pdf read and convertion to txt
+
+public class TxtDealer {
+    private String userPath;
+
+    public TxtDealer(String userPath){
+        this.userPath = userPath;
+    }
+}
